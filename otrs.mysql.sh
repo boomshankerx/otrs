4a4693e58ad0f6f68b0b89ac35da6ecd2c6f45a8
Lorne Guse <boomshankerx@hotmail.com>
2019-07-17 11:40:24 -0600

    Added otrs.mysql.sh to update mariadb configuration

--- otrs.mysql.sh
+++ otrs.mysql.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-VERSION=6.0.20
-ARCHIVE=otrs-$VERSION.tar.gz
-BASE=/opt/otrs
-BACKUP=upgrade/upgrade
 DATE=`date '+%F-%H%M%S'`
-OTRS=otrs-$VERSION
+sed -i -r -e 's/max_allowed_packet\s+= 16M/max_allowed_packet = 64M/' \
+    -e 's/query_cache_size\s+= 16M/query_cache_size = 32M/' \
+    -e '/\[mysqld\]/a innodb_log_file_size = 256M' \
+    /etc/mysql/mariadb.conf.d/50-server.cnf

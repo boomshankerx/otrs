bca635abc2831f4facb06b9ef5018d05f225c9ea
Lorne Guse <boomshankerx@hotmail.com>
2019-08-27 23:20:19 -0600

    Upgraded to v6.0.21

---
+++ otrs.cleanup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DATE=`date '+%F-%H%M%S'`
+VERSION=6.0.21
+ARCHIVE=otrs-$VERSION.tar.gz
+BASE=/opt/otrs
+OTRS=otrs-$VERSION
+BACKUP=upgrade/upgrade-$DATE
+
+
--- otrs.upgrade.sh
+++ otrs.upgrade.sh
@@ -1,11 +1,11 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
 
-VERSION=6.0.20
+DATE=`date '+%F-%H%M%S'`
+VERSION=6.0.21
 ARCHIVE=otrs-$VERSION.tar.gz
 BASE=/opt/otrs
-BACKUP=upgrade/upgrade
-DATE=`date '+%F-%H%M%S'`
 OTRS=otrs-$VERSION
+BACKUP=upgrade/upgrade-$DATE
 
 # DOWNLOAD LATEST
 curl -O https://ftp.otrs.org/pub/otrs/$ARCHIVE
@@ -16,12 +16,12 @@ service postfix stop
 service apache2 stop
 su -c "$BASE/bin/Cron.sh stop" otrs
 su -c "$BASE/bin/otrs.Daemon.pl stop" otrs
-[[ -d $BACKUP ]] || mkdir $BACKUP
+[[ -d $BACKUP ]] || mkdir -p $BACKUP
 cp $BASE/Kernel/Config.pm $BACKUP
 cp -r $BASE/var $BACKUP
 
 # EXTRACT NEW SOURCE
-tar -zxvf $ARCHIVE -C /opt
+tar -zxf $ARCHIVE -C /opt
 rm /opt/otrs
 ln -s /opt/$OTRS /opt/otrs
 
@@ -35,4 +35,3 @@ service postfix start
 service cron start
 su -c "$BASE/bin/otrs.Daemon.pl start" otrs
 su -c "$BASE/bin/Cron.sh start" otrs
-mv $BACKUP upgrade/upgrade-$DATE
